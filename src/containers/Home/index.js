28a23750f05fa654af4a3db7f83afa00e5f67e79
Jarosław Bagnicki <jarek@bagnicki.net>
2019-02-17 13:17:58 +0100

    Project setup

---
+++ src/containers/Home/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from './styles.module.scss';
+
+export class Home extends Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        -- Home container --
+      </div>
+    );
+  }
+}
